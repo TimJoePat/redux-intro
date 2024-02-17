62be33cc75bb82a7a19b35e6a31c1d92115ecef1
TimJoePat <TimJoePat88@cox.net>
2024-02-17 16:31:46 -0700

    265-Adding more state:customer - Complete

--- src/store.js
+++ src/store.js
@@ -1,12 +1,18 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -33,7 +39,32 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer);
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  custom: customerReducer,
+});
+
+const store = createStore(rootReducer);
 
 // store.dispatch({ type: "account/deposit", payload: 500 });
 
@@ -78,4 +109,25 @@ console.log(store.getState());
 store.dispatch(payLoan());
 console.log(store.getState());
 store.dispatch(withdraw(200));
+console.log(store.getState());
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: {
+      fullName,
+      nationalID,
+      createdAt: new Date(), //.toISOString(),
+    },
+  };
+}
+
+function updateName(fullName) {
+  return {
+    type: "accoung/updateName",
+    payload: fullName,
+  };
+}
+
+store.dispatch(createCustomer("Timothy Joe Patterson", 1234567));
 console.log(store.getState());
