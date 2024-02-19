2131b67ca793827f5082ffbe06d31fb6a6fb784b
TimJoePat <TimJoePat88@cox.net>
2024-02-18 23:38:53 -0700

    272 Making API calls with Redux Thunk - Currency Conversion working

--- src/store.js
+++ src/store.js
@@ -1,4 +1,9 @@
-import { combineReducers, createStore } from "redux";
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+} from "redux";
+import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice.js";
 import customerReducer from "./features/customers/customerSlice.js";
 
@@ -7,6 +12,6 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
