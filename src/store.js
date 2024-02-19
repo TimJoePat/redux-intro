5ffd9f872649b48ee0dcbd1929193c0efd96b550
TimJoePat <TimJoePat88@cox.net>
2024-02-19 11:33:57 -0700

    converted to RTK

--- src/store.js
+++ src/store.js
@@ -1,20 +1,12 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  createStore,
-} from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice.js";
 import customerReducer from "./features/customers/customerSlice.js";
-import { composeWithDevTools } from "redux-devtools-extension";
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
-});
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
 
 export default store;
